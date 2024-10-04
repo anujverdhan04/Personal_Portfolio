7810267ffdabe8265948b84e455fc5e974eae246
AnujVerdhan04 <anujverdhan049@gmail.com>
2024-10-04 10:42:18 +0530

    Updated API calls to point to deployed backend

--- portfolio-frontend/src/services/api.js
+++ portfolio-frontend/src/services/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+// Define the API URL constant
+const API_URL = 'https://personal-portfolio-dxah.onrender.com/api/';
+
 // Example API call
 export const exampleApiCall = async () => {
     try {
-        const response = await axios.get('http://127.0.0.1:8000/api/example/');
+        const response = await axios.get(`${API_URL}projects/`); // Using the API_URL constant
         console.log(response.data);
     } catch (error) {
         console.error('Error:', error);
@@ -13,7 +16,7 @@ export const exampleApiCall = async () => {
 // Fetch Projects
 export const getProjects = async () => {
     try {
-        const response = await axios.get('http://127.0.0.1:8000/api/projects/');
+        const response = await axios.get(`${API_URL}projects/`); // Corrected endpoint
         return response.data;  // Return only the data
     } catch (error) {
         console.error('Error fetching projects:', error);  // Log error to console
@@ -24,21 +27,21 @@ export const getProjects = async () => {
 // Fetch Skills
 export const getSkills = async () => {
     try {
-        const response = await axios.get('http://127.0.0.1:8000/api/skills/');
+        const response = await axios.get(`${API_URL}skills/`); // Using the API_URL constant
         return response.data;  // Return only the data
     } catch (error) {
         console.error('Error fetching skills:', error);  // Log error to console
         throw error;  // Re-throw error to handle it in the component
     }
 };
+
 // Fetch Internship
 export const getInternships = async () => {
     try {
-        const response = await axios.get('http://127.0.0.1:8000/api/internships/');
+        const response = await axios.get(`${API_URL}internships/`); // Using the API_URL constant
         return response.data;  // Return only the data
     } catch (error) {
         console.error('Error fetching internships:', error);  // Log error to console
         throw error;  // Re-throw error to handle it in the component
     }
 };
-
