8a8b46ba09e87b827deafab8cd3bf34ea80dbce3
AnujVerdhan04 <anujverdhan049@gmail.com>
2024-10-04 10:48:08 +0530

    Updated API calls to point to deployed backend

--- portfolio-frontend/src/services/api.js
+++ portfolio-frontend/src/services/api.js
@@ -1,22 +1,13 @@
+// api.js
 import axios from 'axios';
 
-// Define the API URL constant
+// Set the base URL for your API
 const API_URL = 'https://personal-portfolio-dxah.onrender.com/api/';
-
-// Example API call
-export const exampleApiCall = async () => {
-    try {
-        const response = await axios.get(`${API_URL}projects/`); // Using the API_URL constant
-        console.log(response.data);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-};
 
 // Fetch Projects
 export const getProjects = async () => {
     try {
-        const response = await axios.get(`${API_URL}projects/`); // Corrected endpoint
+        const response = await axios.get(`${API_URL}projects/`);
         return response.data;  // Return only the data
     } catch (error) {
         console.error('Error fetching projects:', error);  // Log error to console
@@ -27,7 +18,7 @@ export const getProjects = async () => {
 // Fetch Skills
 export const getSkills = async () => {
     try {
-        const response = await axios.get(`${API_URL}skills/`); // Using the API_URL constant
+        const response = await axios.get(`${API_URL}skills/`);
         return response.data;  // Return only the data
     } catch (error) {
         console.error('Error fetching skills:', error);  // Log error to console
@@ -38,7 +29,7 @@ export const getSkills = async () => {
 // Fetch Internship
 export const getInternships = async () => {
     try {
-        const response = await axios.get(`${API_URL}internships/`); // Using the API_URL constant
+        const response = await axios.get(`${API_URL}internships/`);
         return response.data;  // Return only the data
     } catch (error) {
         console.error('Error fetching internships:', error);  // Log error to console
